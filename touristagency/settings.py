308ed448c146f462c4126577d96c99617f6fb001
Srecko Kreckovic <113716434+sreckokreckovic@users.noreply.github.com>
2023-09-28 19:12:28 +0200

    db settings update

--- touristagency/settings.py
+++ touristagency/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'tourist_agency_db',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'mysql.server.pythonanywhere-services.com',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'STRICT_ALL_TABLES',
