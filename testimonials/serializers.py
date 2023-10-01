f49dfd36360149b409e909c4a053138f5c8d26cc
Srecko Kreckovic <113716434+sreckokreckovic@users.noreply.github.com>
2023-10-01 15:09:14 +0200

    Remove field title from testimonial

--- testimonials/serializers.py
+++ testimonials/serializers.py
@@ -24,7 +24,6 @@ class TestimonialSerializer(serializers.ModelSerializer):
         model = Testimonial
         fields = [
             'id',
-            'title',
             'description',
             'user',
         ]
